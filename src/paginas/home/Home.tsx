9496d680d2f8cdf5a1a091fe382acbf1e709e856
Kiev <kevinkiss2010@gmail.com>
2024-01-15 19:27:46 -0300

    Adicionando front com react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_0.jpg" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;

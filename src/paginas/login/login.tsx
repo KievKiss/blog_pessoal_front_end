814e16c0a1cd6baa034f4a7fa3a94fbdbc276b10
Kiev <kevinkiss2010@gmail.com>
2024-01-22 10:41:05 -0300

    Criando cadastro de temas e suas modificações

--- src/paginas/login/login.tsx
+++ src/paginas/login/login.tsx
@@ -65,7 +65,7 @@ function login(e: ChangeEvent<HTMLFormElement>) {
               onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
-          <button  type='submit' className="rounded bg-indigo-400 hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
+          <button  type='submit' className="rounded bg-[#3a135c] hover:bg-indigo-400 text-white w-1/2 py-2 flex justify-center">
            {isLoading ? <RotatingLines
             strokeColor="white"
             strokeWidth="5"
@@ -80,7 +80,7 @@ function login(e: ChangeEvent<HTMLFormElement>) {
 
           <p>
             Ainda não tem uma conta?{' '}
-            <Link to="/cadastro" className="text-indigo-800 hover:underline">
+            <Link to="/cadastro" className="text-[#3a135c] hover:underline">
               Cadastre-se
             </Link>
           </p>

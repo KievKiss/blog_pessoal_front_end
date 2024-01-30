a0107089e9a8c65cf08943f29d3a66591f8209c9
Kiev <kevinkiss2010@gmail.com>
2024-01-30 12:28:00 -0300

    Adicionando Alerts personalizados

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,28 +5,29 @@ import ListaPostagens from '../../components/postagens/listaPostagens/ListaPosta
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
 
+
 function Home() {
     return (
         <>
-        <div className="bg-[#3a135c] flex justify-center">
+        <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Feliz por ter você aqui.</h2>
+              <h2 className='text-5xl font-bold'>Feliz por ter vcê aqui!</h2>
               <p className='text-xl'>Sinta-se em casa</p>
   
               <div className="flex justify-around gap-4">
               <ModalPostagem />
-                <button className='border rounded px-4 hover:bg-white hover:text-indigo-800'>Ver postagens</button>
-              </div>
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
   
             <div className="flex justify-center ">
-            <img src={homeLogo} alt="" className='w-2/3' />      
+              <img src={homeLogo} alt="" className='w-2/3' />
+      
             </div>
-          </div>          
+          </div>
         </div>
         <ListaPostagens />
-      
       </>
     );
 }
